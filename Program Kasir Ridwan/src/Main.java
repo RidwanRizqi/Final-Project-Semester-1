6aa89ced0124058590ba47e5e7dbfd0ae9ec99c8
RidwanRizqi <Ridwanrizqi64@gmail.com>
2021-12-04 11:51:57 +0700

    hari ke 2

--- Program Kasir Ridwan/src/Main.java
+++ Program Kasir Ridwan/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -18,6 +19,16 @@ public class Main {
             {10,10,10,10,10,10,10,10,10,10},     // tiket premium
             {10,10,10,10,10,10,10,10,10,10}};    // tiket reguler
     static int[] daftarKursi = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    static String[] riwayatPembeli = new String[100];
+    static String[][] riwayatFilm = new String[20][100];
+    static int[][] riwayatTiket = new int[2][100];
+    static int[][] riwayatKursi = new int[20][100];
+    static float[] riwayatTotal = new float[100];
+    static String[] daftarDiskonMember = new String[100];
+    static int nomorUrut = 0;
+    static int i = 0;
+    static int totalTiketDipesan = 0;
+
     public static void main(String[] args) {
 
             System.out.println("============================================");
@@ -44,12 +55,11 @@ public class Main {
             System.out.println("============================================");
             System.out.print("Silahkan memilih menu diatas dengan cara memasukkan angka 1/2 : ");
             menu = input.nextInt();
+            System.out.println();
             switch (menu) {
                 case 1 -> pelanggan();
                 case 2 -> pegawai();
-                default -> {
-                    System.out.println("Masukkan yang anda masukkan salah!");
-                }
+                default -> System.out.println("Masukkan yang anda masukkan salah!");
             }
             System.out.println("Apakah masih ada keperluan lain? (Ya/Tidak) : ");
             konfirmasi = input.next();
@@ -79,8 +89,9 @@ public class Main {
             System.out.println("|  | [6] Kembali ke Menu Sebelumnya     |  |");
             System.out.println("|  |====================================|  |");
             System.out.println("============================================");
-            System.out.print("Silahkan memilih menu diatas dengan cara memasukkan angka 1 sampai 5 : ");
+            System.out.print("Silahkan memilih menu diatas dengan cara memasukkan angka 1 sampai 6 : ");
             menu = input.nextInt();
+            System.out.println();
             switch (menu) {
                 case 1 -> pembelianTiket();
                 case 2 -> daftarFilm();
@@ -116,13 +127,14 @@ public class Main {
         System.out.println("|  | [6] Kembali ke Menu Sebelumnya     |  |");
         System.out.println("|  |====================================|  |");
         System.out.println("============================================");
-        System.out.print("Silahkan memilih menu diatas dengan cara memasukkan angka 1 sampai 5 : ");
+        System.out.print("Silahkan memilih menu diatas dengan cara memasukkan angka 1 sampai 6 : ");
         menu = input.nextInt();
+        System.out.println();
         switch (menu) {
             case 1 -> updateStockTiket();
             case 2 -> updateKursi();
             case 3 -> updateFilm();
-            case 4 -> riwayat();
+            case 4 -> cetakRiwayat();
             case 5 -> daftarNomorMember();
             case 6 -> menu();
             default -> System.out.println("Mohon maaf input yang anda masukkan salah");
@@ -146,10 +158,10 @@ public class Main {
     private static void updateKursi() {
         kursiReady();
         for (int i = 0; i < daftarKursi.length; i++) {
-            daftarKursi[i] = i;
+            daftarKursi[i] = (i + 1);
         }
-        System.out.println("Jumlah kursi sudah ready semua ");
         kursiReady();
+        System.out.println("Jumlah kursi sudah ready semua ");
     }
 
     private static void updateFilm() {
@@ -164,10 +176,6 @@ public class Main {
         daftarFilm();
     }
 
-    private static void riwayat() {
-
-    }
-
     private static void daftarNomorMember() {
 
     }
@@ -176,10 +184,10 @@ public class Main {
         String nama, member, jenisTiket, konfirmasiPesanLagi;
         int[] filmDiPesan = new int[10];
         int[][] tiketDiPesan = new int[2][10];
-        int[] kursiDiPesan = new int[20];
+        int[][] kursiDiPesan = new int[20][20];
         float diskonMember = 0, total = 0;
         boolean kondisi = true;
-        int i = 0, totalTiketDipesan = 0;
+        int[] counter = new  int[20];
 
         System.out.println("============================================");
         System.out.println("|============  Pembelian Tiket  ===========|");
@@ -190,17 +198,19 @@ public class Main {
         member = input.next();
         if (member.equalsIgnoreCase("Ya")) {
             diskonMember = 0.2f;
+            daftarDiskonMember[nomorUrut] = "20%";
         } else if (member.equalsIgnoreCase("Tidak")) {
             diskonMember = 0;
+            daftarDiskonMember[nomorUrut] = "0%";
         } else {
             System.out.println("Masukkan yang anda masukkan salah! silahkan mengulang proses pembelian tiket!");
             pembelianTiket();
         }
         while (kondisi) {
+            System.out.println();
             daftarFilm();
             System.out.print("Masukkan film yang ingin anda tonton sesuai dengan nomor nya 1 sampai 10 : ");
             filmDiPesan[i] = input.nextInt() - 1;
-            System.out.println("Tersedia " + stockTicket[0][i] + " tiket premium dan " + stockTicket[1][i] + " tiket reguler untuk film " + daftarFilm[filmDiPesan[i]]);
             hargaTiket();
             System.out.println("Tiket jenis apa yang ingin anda beli ? (premium/reguler) :");
             jenisTiket = input.next();
@@ -216,23 +226,24 @@ public class Main {
                 System.out.println("Masukkan yang anda masukkan salah! silahkan mengulangi proses dari awal!");
                 pembelianTiket();
             }
+            counter[i] = tiketDiPesan[0][i] + tiketDiPesan[1][i];
             totalTiketDipesan += tiketDiPesan[0][i] + tiketDiPesan[1][i];
             kursiReady();
-            System.out.println("Di atas adalah daftar kursi kita yang ready angka 0 berarti tidak tersedia");
-            for (int j = 0; j < totalTiketDipesan; j++) {
+            System.out.println("Di atas adalah daftar kursi kita yang ready ");
+            for (int j = 0; j < counter[i]; j++) {
                 System.out.println("Silahkan masukkan nomor kursi untuk memesan kursis tiket ke " + (j+1) + " : ");
-                kursiDiPesan[j] = input.nextInt();
+                kursiDiPesan[i][j] = input.nextInt();
                 for (int k = 0; k < daftarKursi.length; k++) {
-                    if (daftarKursi[k] == kursiDiPesan[j]) {
+                    if (daftarKursi[k] == kursiDiPesan[i][j]) {
                         daftarKursi[k] = 0;
                     }
                 }
+                riwayatKursi[j][nomorUrut] = kursiDiPesan[i][j];
             }
 
             System.out.println("Apakah anda ingin memesan tiket lagi ? (Ya/Tidak) :");
             konfirmasiPesanLagi = input.next();
             if (konfirmasiPesanLagi.equalsIgnoreCase("Ya")) {
-                kondisi = true;
                 i++;
             } else if (konfirmasiPesanLagi.equalsIgnoreCase("Tidak")) {
                 kondisi = false;
@@ -244,38 +255,58 @@ public class Main {
             total += (tiketDiPesan[0][j] * 75_000);
             total += (tiketDiPesan[1][j] * 50_000);
         }
+        riwayatTotal[nomorUrut] = total;
+        System.out.println();
         System.out.println("Berikut adalah rincian pembayaran yang harus dibayarkan : ");
         System.out.println("=========================================================");
         System.out.println("Nama : " + nama);
         for (int j = 0; j < (i + 1); j++){
             System.out.println("Judul Film : " + daftarFilm[filmDiPesan[j]]);
-            System.out.println(tiketDiPesan[0][j] + " tiket premium");
-            System.out.println(tiketDiPesan[1][j] + " tiket reguler");
+            if (tiketDiPesan[0][j] != 0) {
+                System.out.println(tiketDiPesan[0][j] + " tiket premium");
+                riwayatTiket[0][nomorUrut] += tiketDiPesan[0][j];
+            }
+            if (tiketDiPesan[1][j] != 0) {
+                System.out.println(tiketDiPesan[1][j] + " tiket reguler");
+                riwayatTiket[1][nomorUrut] += tiketDiPesan[1][j];
+            }
+            System.out.print("Nomor kursi : ");
+            for (int k = 0; k < counter[i]; k++) {
+                if (kursiDiPesan[j][k] != 0) {
+                    System.out.print(kursiDiPesan[j][k] + ", ");
+                }
+            }
+            System.out.println();
             System.out.println("=========================================================");
         }
-        System.out.println("Nomor kursi : ");
-        for (int j = 0; j < totalTiketDipesan; j++) {
-            System.out.print(kursiDiPesan[j] + ", " );
-        }
-        System.out.println();
         System.out.println("Diskon member sebesar " + diskonMember);
         System.out.println("Diskon = " + total * diskonMember);
         System.out.println("=========================================================");
         System.out.println("Total : " + (total - (total * diskonMember)));
         System.out.println("=========================================================");
+
+        // rekap
+        riwayatPembeli[nomorUrut] = nama;
+        for (int j = 0; j < (i + 1); j++) {
+            riwayatFilm[j][nomorUrut] = daftarFilm[filmDiPesan[j]];
+        }
+        nomorUrut++;
     }
 
     private static void daftarFilm(){
+        System.out.println();
         System.out.println("============================================");
         System.out.println("|========  Daftar Film Minggu Ini  ========|");
         System.out.println("============================================");
         for (int i = 0; i < daftarFilm.length; i++){
             System.out.println("[" + (i + 1) + "]" + " " + daftarFilm[i]);
-            System.out.println("Stock tiket premium = " + stockTicket[0][i] + " Stock tiket reguler = " + stockTicket[1][i]);
+            System.out.println("    Stock tiket premium = " + stockTicket[0][i]);
+            System.out.println("    Stock tiket reguler = " + stockTicket[1][i]);
         }
     }
 
     private static void kursiReady(){
+        System.out.println();
         System.out.println("============================================");
         System.out.println("|==========  Kursi Yang Tersedia  =========|");
         System.out.println("============================================");
@@ -283,12 +314,15 @@ public class Main {
             if (i%4 == 0 && i != 0) {
                 System.out.println();
             }
-            System.out.print(daftarKursi[i] + " ");
+            if (daftarKursi[i] != 0) {
+                System.out.print(daftarKursi[i] + "\t");
+            }
         }
         System.out.println();
     }
 
     private static void hargaTiket(){
+        System.out.println();
         System.out.println("============================================");
         System.out.println("|=============  Harga Tiket   ==============|");
         System.out.println("============================================");
@@ -301,5 +335,36 @@ public class Main {
         System.out.println("============================================");
         System.out.println("|=========  Daftar Sebagai Member  ========|");
         System.out.println("============================================");
+    }
+
+    private static void cetakRiwayat() {
+        System.out.println("============================================");
+        System.out.println("============  Riwayat Transaksi  ===========");
+        System.out.println("============================================");
+        System.out.println("============================================");
+        for (int j = 0; j < nomorUrut; j++) {
+            System.out.println("========  Transaksi ke " + (j+1) + "  ========");
+            System.out.println("Nama : " + riwayatPembeli[j]);
+            System.out.println("Judul Film :");
+            for (String[] strings : riwayatFilm) {
+                if (strings[j] != null) {
+                    System.out.println(strings[(j)]);
+                }
+            }
+            System.out.println("Tiket : ");
+            System.out.println(riwayatTiket[0][j] + " tiket premium");
+            System.out.println(riwayatTiket[1][j] + " tiket reguler");
+            System.out.println("Nomor kursi : ");
+            for (int k = 0; k < totalTiketDipesan; k++) {
+                if (riwayatKursi[k][j] != 0) {
+                    System.out.print(riwayatKursi[k][j] + ", ");
+                }
+            }
+            System.out.println();
+            System.out.println("Diskon member : " + daftarDiskonMember[j]);
+            System.out.println("Total : " + riwayatTotal[j]);
+            System.out.println("============================================");
+            System.out.println();
+        }
     }
 }
